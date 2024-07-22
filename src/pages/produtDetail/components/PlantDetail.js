7df2f670508bfd08709cc3c1cfccb21e327a8ef3
shivrajinnovative <shivraj.innovative@gmail.com>
2024-07-22 11:39:55 +0530

    product detail chnages

--- src/pages/produtDetail/components/PlantDetail.js
+++ src/pages/produtDetail/components/PlantDetail.js
@@ -20,7 +20,7 @@ export default function PlantDetail({ equipment }) {
               return (
                 <div className="col-sm-6 col-md-4 col-lg-3 py-3 py-md-5 border-bottom" key={key}>
                   <p className="fw-400 text-capitalize">{modifiedKey}</p>
-                  <p className="fw-500">{equipment[key]}</p>
+                  <p className="fw-500" dangerouslySetInnerHTML={{ __html: equipment[key] }} ></p>
                 </div>
               );
             }
